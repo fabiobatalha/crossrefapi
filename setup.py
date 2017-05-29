881d7786de1cab2f3c3f9799b634431de5f1281a
fabiobatalha <fabiobatalha@gmail.com>
2017-05-29 13:34:54 -0300

    Journal sample

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ tests_require = []
 
 setup(
     name="crossrefapi",
-    version="1.0.0",
+    version="1.0.1",
     description="Library that implements the endpoints of the Crossref API",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
